443066b502e4f1b967cf8390b914120c52f520da
Raj Vardhan <rajvardaan130@gmail.com>
2025-04-07 18:04:53 +0530

    completed

--- client/src/config/api.jsx
+++ client/src/config/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4500", //"http://20.3.137.135/api" "http://localhost:4500",
+  baseURL: "http://20.3.137.135/api", //"http://20.3.137.135/api" "http://localhost:4500",
   withCredentials: true,
   //timeout: 60000, // 60 seconds timeout
 });
