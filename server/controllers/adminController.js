10a684d4647e9f691facf17790aa84e351bfb2b7
Raj Vardhan <errvmaurya563@gmail.com>
2025-06-17 22:03:13 +0530

    update in admin controller

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -181,12 +181,12 @@ export const deleteGalleryPicture = async (req, res) => {
     if (!image) {
       return res.status(404).json({ message: "Image not found" });
     }
-    console.log("Image to delete:", image);
+    console.log("Image to delete:", image.imageUrl);
     if (image.imageUrl) {
       const imgId = image.imageUrl.split("/upload/")[1].split("/");
       console.log("Image ID parts:", imgId);
-      const publicId = imgId[1] + "/" + imgId[2] + "/" + imgId[3].split(".")[0];
-
+      const publicId = imgId[0] + "/" + imgId[1] + "/" + imgId[2].split(".")[0];
+      console.log("Public ID for deletion:", publicId);
       try {
         if (publicId) {
           const result = await cloudinary.uploader.destroy(publicId);
