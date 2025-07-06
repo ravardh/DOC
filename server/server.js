404225cab34dae7bbc1729c79290340cacd5ae77
Raj Vardhan <errvmaurya563@gmail.com>
2025-07-06 16:15:25 +0530

    Route Added

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({origin: ["http://dropsofchange.in", "http://www.dropsofchange.in"], credentials: true}));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(morgan("dev"));
